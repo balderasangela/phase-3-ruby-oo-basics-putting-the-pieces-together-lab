6a0ed06b62595ca9841f5f836f6b1fe209dc8ed1
Angela <balderasangela@gmail.com>
2023-02-01 14:20:43 -0500

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count
 
+   
+
+    def initialize(title)
+        @title = title
+        @author = author
+        @genre = genre
+       @page_count = page_count
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+attr_accessor :brand, :size, :color, :material, :condition
+# attr_writer :size
+
+    def initialize(brand)
+        @brand = brand
+        @size = size
+        @color = color
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
